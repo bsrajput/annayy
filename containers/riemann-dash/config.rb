3bbfdb67f7cc2b4f245273ac094ee5fbed70e8ed
Abhishek Anand Amralkar <abhishekamralkar@pm.me>
2018-12-20 21:25:29 -0500

    adding the Changelog file

--- containers/riemann-dash/config.rb
+++ containers/riemann-dash/config.rb
@@ -1,1 +1,4 @@
-../../configs/riemanndash_config.rb
+# Serve HTTP traffic on this port
+set  :port, 4567
+set :bind, "0.0.0.0"
+config.store[:ws_config] = "~/Code/Clojure/annayy/containers/riemann-dash/config.json"
